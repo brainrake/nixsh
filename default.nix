4bd1c45592ae183ad491505236626aca36ab7818
Márton Boros <martonboros@gmail.com>
2022-06-04 13:39:22 +0300

    update

--- default.nix
+++ default.nix
@@ -1,12 +1,19 @@
 { pkgs ? import <nixpkgs> { }, ... }:
 {
+  # nix repl will not run shellHook on mkShell
   run = command: pkgs.stdenv.mkDerivation {
     name = "run";
-    src = ./.;
-    installPhase = "mkdir $out";
+
+    # do not require src
+    unpackPhase = "true";
+
     shellHook = ''
+      # nix repl only shows stderr, so redirect stdout to stderr
       exec 1>&2
+
       ${command}
+
+      # return to nix repl
       exit 0
     '';
   };
